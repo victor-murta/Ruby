367e970bcf528463fa1c3ebde402f327b7486f83
victor-murta <vmurtag16@gmail.com>
2021-04-01 11:51:19 -0300

    Iniciando com Ruby

---
+++ DataStructure/EstruturaDeDados.rb
@@ -0,0 +1,41 @@
+# STRINGS
+#comentário de 1 linha
+# h = 'Hello world '
+# nome = 'Victor Murta'
+# sobrenome = %q(Murta)
+# mensagem = <<~MENSAGEM 
+
+#     Olá #{nome},
+#     Seja bem vindo ao Ruby
+
+# MENSAGEM
+# puts mensagem.leng
+# puts "Bem vindo #{nome}" #com aspas simples não funciona
+# puts sobrenome
+
+# OPERAÇÕES COM STRING
+# puts '-'*10
+# c = 'Curso de Ruby'
+# puts c[0]
+# puts c[0,10]
+
+# c = '  Curso de Ruby'
+# puts c.chars
+# puts c.strip
+# puts c.upcase
+# puts c.downcase
+# puts c.captalize
+
+c = "Victor Murta"
+d = "Victor-Murta"
+
+# puts = quebra a linha automaticamente
+# print = não quebra a linha
+# p é a mesma coisa do print não quebra a linha
+p d[- 1]
+p c.chars
+# puts c.split
+# print(c.split)
+# print c.split
+# p c.split
+# puts d.split("-")
